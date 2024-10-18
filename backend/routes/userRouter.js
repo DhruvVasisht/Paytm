b35166a5ed73aad1e5550b10abf5b3d516c0ebd3
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-10-18 11:57:03 +0530

    Day 2

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,88 @@
 const express = require('express');
 const router = express.Router();
+const zod = require('zod');
+const jwt = require("jsonwebtoken");
+const {User} = require('../models/db');
+const JWT_SECRET = require('../config/config');
 
-router.post("/signup", (req,res)=>{
+const signedUpSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+});
+router.post("/signup", async(req,res)=>{
+    const body = req.body;
+    const {success} = signedUpSchema.safeParse(req.body);
+    if(!success){
+        return res.json({
+            message: "Email Already Taken / Incorrect Inputs"
+        })
+    }
 
+    const existingUser = await User.findOne({
+        username: body.username
+    })
+
+    if(existingUser){
+        return res.json({
+            message: "Email Already Taken / Incorrect Inputs"
+        })
+    }
+
+    const user = await User.create({
+        username: body.username,
+        password: body.password,
+        firstName: body.firstName,
+        lastName: body.lastName
+    });
+    
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.json({
+        message: "User Created Successfully",
+        token: token
+    })
 })
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+	password: zod.string()
+})
+
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+  
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+});
 
 module.exports = router;
