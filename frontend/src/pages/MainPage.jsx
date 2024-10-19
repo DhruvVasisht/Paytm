6d6ef74367a51fa1123b56df371339156d031e6f
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-10-19 15:55:24 +0530

    FE+BE Integration

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Balance from '../components/Balance';
+import AppBar from '../components/AppBar';
+import Users from '../components/Users';
 
 function MainPage() {
   return (
-    <div>MainPage</div>
-  )
+    <div>
+      <AppBar/>
+      <div className='m-8'>
+        <Balance value={"10,000"} />
+        <Users/>
+      </div>
+    </div>
+  );
 }
 
-export default MainPage
+export default MainPage;
